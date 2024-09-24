1c7102a3a3b8580a1900b57c17cbdb03d59af225
Shubhajit <shubjitgithub@gmail.com>
2024-09-24 18:49:15 +0530

    Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useState } from "react";
 function App() {
+  const [authUser,setAuthuser]=useState(true)
   return (
-	<div>App</div>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      {/* <Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+
+     
+      </Routes> */}
+         <Login/>
+      <Toaster />
+
+    </div>
   )
 }
 
