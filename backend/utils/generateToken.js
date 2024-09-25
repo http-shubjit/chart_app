6e80ecf319cc302e45315a56ac3bcdfcedf9f156
Shubhajit <shubjitgithub@gmail.com>
2024-09-25 08:33:10 +0530

    Login signup method implement

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks//cannot acces by document.cookie
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks//cannnot acces when you scroll in your browser and click on the image then that image conatin post request which will go to another browser and someone can acces your token
+		sameSite: "strict", // CSRF attacks cross-site request forgery attacks//cannnot acces when you scroll in your browser and click on the image then that image url conatin post method request which will go to server of an attacker and attacker can not acces it by using 	const token = req.cookies.jwt; like this
 		secure: process.env.NODE_ENV !== "development",
 	});
 };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,20 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { useAuthContext } from "./context/AuthContext";
+
 function App() {
-  const [authUser,setAuthuser]=useState(true)
-  return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Routes>
+	const { authUser } = useAuthContext();
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
 				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
-
-     
-      </Routes> */}
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      <Home/>
-      <Toaster />
-
-    </div>
-  )
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
-export default App
+export default App;
